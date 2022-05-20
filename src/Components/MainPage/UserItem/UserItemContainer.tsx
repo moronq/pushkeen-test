0908f6ee4c2b11dfa0020131d0c86c72f38d6660
moronq <moronqq@gmail.com>
2022-05-20 23:23:51 +0300

    useEffect works twice?

--- src/Components/MainPage/UserItem/UserItemContainer.tsx
+++ src/Components/MainPage/UserItem/UserItemContainer.tsx
@@ -1,21 +1,30 @@
 import React, {useEffect} from 'react';
 import UserItem from "./UserItem";
-import {useAppDispatch} from "../../../hook/hook";
+import {useAppDispatch, useAppSelector} from "../../../hook/hook";
 import {fetchUsers} from "../../../store/Reducers/UserSlice";
 
 const UserItemContainer = () => {
 
     const dispatch = useAppDispatch()
+    const {status} = useAppSelector(state => state.userReducer)
 
-    useEffect(()=>{
-        dispatch(fetchUsers())
+    //
+    // useEffect(()=>{
+    //     console.log('empty logic')
+    // },[])
+
+    useEffect(() => {
+        if (status === 'idlk') {
+            dispatch(fetchUsers())
+        }
     }, [])
 
     return (
         <div>
             <UserItem/>
         </div>
-    );
-};
+    )
+}
+
 
 export default UserItemContainer;
--- src/store/Reducers/UserSlice.ts
+++ src/store/Reducers/UserSlice.ts
@@ -1,42 +1,43 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {UsersAPI} from "../../api/api";
+import {GetUsersType} from "../../types/UsersTypes";
 
 type initialStateType = {
-    status: string
-    usersList:any
+    status: 'idlk' | 'loading' | 'succeeded' | 'failed'
+    usersList: Array<GetUsersType>
+    error: string | undefined
 }
+
 
 const initialState:initialStateType = {
     usersList: [],
-    status: ''
+    status: 'idlk',
+    error: ''
 }
 
 export const userSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {
+
     },
-    // extraReducers(builder) {
-    //     builder
-    //         .addCase(fetchComments.pending, (state)=>{
-    //             state.isLoading = true
-    //             state.status = 'loading'
-    //         })
-    //         .addCase(fetchComments.fulfilled, (state, action: any) => {
-    //             state.isLoading = false
-    //             state.status = 'succeeded'
-    //             state.comments = state.comments.concat(action.payload)
-    //         })
-    //         .addCase(fetchComments.rejected, (state, action) => {
-    //             state.isLoading = false
-    //             state.status = 'failed'
-    //             state.error = action.error.message
-    //         })
-    // }
+    extraReducers(builder) {
+        builder
+            .addCase(fetchUsers.pending, (state)=>{
+                state.status = 'loading'
+            })
+            .addCase(fetchUsers.fulfilled, (state, action: any) => {
+                state.status = 'succeeded'
+                state.usersList = state.usersList.concat(action.payload)
+            })
+            .addCase(fetchUsers.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
+    }
 })
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async ()=>{
-    console.log("dispatched")
     return await UsersAPI.getUsers()
 })
 
