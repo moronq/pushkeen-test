445ad62a747e1b7217c60da5f2b6797a87eeaa5a
moronq <moronqq@gmail.com>
2022-05-22 14:56:22 +0300

    fixed second page

--- src/Components/UserPage/UserPage.tsx
+++ src/Components/UserPage/UserPage.tsx
@@ -4,6 +4,7 @@ import styles from './UserPage.module.scss'
 import {useAppDispatch, useAppSelector} from "../../hook/hook";
 import PostList from "./PostList/PostList";
 import {fetchUserInfo, fetchUserPost, userSlice} from "../../store/Reducers/UserSlice";
+import UserInfo from "./UserInfo/UserInfo";
 
 const UserPage = () => {
 
@@ -32,46 +33,24 @@ const UserPage = () => {
     }, [])
 
 
-    // let contentInfo
-    //
-    // if (statusInfo === 'loading') {
-    //     contentInfo = <p>Загрузка...</p>
-    // } else if (statusInfo === 'succeeded') {
-    //     contentInfo = <section className={styles.userPageBody}>
-    //         <div className={styles.userNameWrapper}>
-    //             <h5 className={styles.userName}>{userInfo.username}</h5>
-    //         </div>
-    //         <div className={styles.userInfoWrapper}>
-    //             <ul className={styles.userInfoList}>
-    //                 <li className={styles.userInfoItem}>{userInfo.name}</li>
-    //                 <li className={styles.userInfoItem}>{userInfo.email}</li>
-    //                 <li className={styles.userInfoItem}>{userInfo.phone}</li>
-    //                 <li className={styles.userInfoItem}>{userInfo.website}</li>
-    //                 <li className={styles.userInfoItem}>{userInfo.company.name}</li>
-    //                 <li className={styles.userInfoItem}>{userInfo.company.bs}</li>
-    //             </ul>
-    //         </div>
-    //     </section>
-    // } else if (statusInfo === 'failed') {
-    //     contentInfo = <p>{errorInfo}</p>
-    // }
+    let contentInfo
+
+    if (statusInfo === 'loading' || statusPost === 'loading') {
+        contentInfo = <p>Загрузка...</p>
+    } else if (statusInfo === 'succeeded' && statusPost === 'succeeded') {
+        contentInfo = <>
+            <UserInfo userInfo={userInfo}/>
+            <PostList userPosts={userPosts} countPost={COUNT_POST}/>
+        </>
+    } else if (statusInfo === 'failed') {
+        contentInfo = <p>{errorInfo}</p>
+    } else if (statusPost === 'failed'){
+        contentInfo = <p>{errorPost}</p>
+    }
     return (
         <main className={styles.userPageContainer}>
             <div className={styles.userPageBody}>
-                <div className={styles.userNameWrapper}>
-                    <h5 className={styles.userName}>{userInfo.username}</h5>
-                </div>
-                <div className={styles.userInfoWrapper}>
-                    <ul className={styles.userInfoList}>
-                        <li className={styles.userInfoItem}>{userInfo.name}</li>
-                        <li className={styles.userInfoItem}>{userInfo.email}</li>
-                        <li className={styles.userInfoItem}>{userInfo.phone}</li>
-                        <li className={styles.userInfoItem}>{userInfo.website}</li>
-                        {/*<li className={styles.userInfoItem}>{userInfo.company.name}</li>*/}
-                        {/*<li className={styles.userInfoItem}>{userInfo.company.bs}</li>*/}
-                    </ul>
-                </div>
-                <PostList userPosts={userPosts} countPost={COUNT_POST}/>
+                {contentInfo}
             </div>
         </main>
     )
